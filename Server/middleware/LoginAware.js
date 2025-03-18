1f773ed3a3831c328a4b7cafcf4f8d1468d25878
wesley <stalinw7@gmail.com>
2025-03-17 22:09:21 -0400

    fixed token condition

--- Server/middleware/LoginAware.js
+++ Server/middleware/LoginAware.js
@@ -4,11 +4,11 @@ const JWT_TOKEN = "goofd";
 const LoginAware = (req, res, next) => {
   const token = req.header("authToken");
 
-  // if (!token) {
-  //   return res
-  //     .status(401)
-  //     .send({ Success: false, errors: [{ msg: "invalid token" }] });
-  // }
+  if (!token) {
+    return res
+      .status(401)
+      .send({ Success: false, errors: [{ msg: "invalid token" }] });
+  }
   const data = jwt.verify(token, JWT_TOKEN);
   if (!data) {
     return res
